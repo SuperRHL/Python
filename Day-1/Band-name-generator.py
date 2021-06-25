4c68289b3cad13141576b64793ada81b20ecc726
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-06-26 00:15:11 +0530

    Band name generator final exercise

---
+++ Day-1/Band-name-generator.py
@@ -0,0 +1,8 @@
+#Band name generator
+print("Band name generator")
+
+city = input("Where did you grow up? ")
+pet = input("What is the name of your pet? ")
+
+print("Your band name is...")
+print(city+pet)
