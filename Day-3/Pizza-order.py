4b28e1f127e9078c5cdfec5c6d67161a2026e78b
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-06-26 12:42:26 +0530

    pizzapy!!

---
+++ Day-3/Pizza-order.py
@@ -0,0 +1,27 @@
+print("Welcome to Pizzapy deliveries!")
+
+size = input("What size pizza do you want? S, M or L. ")
+add_pepperoni = input("Do you want pepperoni? Y or N. ")
+extra_cheese = input("Do you want extra cheese? Y or N. ")
+
+price = 0
+
+if size == "S":
+    price=15
+    
+if size == "M":
+    price=20
+
+if size == "L":
+    price=25
+
+if add_pepperoni == "Y":
+        if size == "S":
+            price+=2
+        else:
+            price+=3
+        
+if extra_cheese == "Y":
+    price+=1
+
+print(f"Please proceed to pay ${price}")
