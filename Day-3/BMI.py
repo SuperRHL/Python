f5e77be28e441f9769715b0da708af957b339f8d
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-06-26 12:14:00 +0530

    refractor

--- Day-3/BMI.py
+++ Day-3/BMI.py
@@ -1,6 +1,6 @@
 height = float(input("What is your height in m? "))
 weight = float(input("What is your weight in kg? "))
-bmi = round(weight/(height*height),2)
+bmi = round(weight/(height ** 2),2)
 
 if bmi < 18.5:
     print(f"Your BMI is {bmi} and you are underweight!")
